f636d557218b2a7ec4e47e8ea75fc37168d351d1
Mudassirshah0627 <mudassirshah0627@gmail.com>
2024-05-23 15:29:58 -0700

    aw-gemmy

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <h1>home</h1>
+      <Button>Hello</Button>
+    </div>
+  );
+}
+
+export default page;
